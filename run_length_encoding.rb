0190ba871453886467399c7c0ca772679c047966
Benjamin Poon <benjamintpoon@gmail.com>
2014-07-19 17:46:58 -0500

    run_length_problem [not working]

---
+++ run_length_encoding.rb
@@ -0,0 +1,29 @@
+
+# Given an input string consisting of repeating characters, return a string 
+# encoded with the number of occurrences of a character followed by the character.
+# e.g.
+# input: 'aabbbcddaa'
+# returns -> '2a3b1c2d2a'
+
+# 'aa'
+# -> '2a'
+
+def run_length(input)
+	index = 0
+	count = 0
+	new_string = ''
+	while index < input.length
+		char = input[index]
+		if (!input[index + 1])
+			new_string.push(count + char)
+		elsif (input[index + 1] == char)
+			index += 1
+			count += 1
+		else
+			# push count and char to new_string
+			count = 0
+			index += 1
+		end
+	end
+	new_string
+end
